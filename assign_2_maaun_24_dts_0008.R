9ddcfff672ccdb55a595331bc0c465c3755e17f6
assiddeeq41-jpg <assiddeeq41@gmail.com>
2025-09-05 14:06:22 +0100

    Add files via upload

---
+++ assign_2_maaun_24_dts_0008.R
@@ -0,0 +1,31 @@
+#This is comment
+x<-5 # Assign 5 to x
+
+x<-10 # numeric (treated as double by default)
+y<-10.5 # numeric (double/decimal)
+
+a<-5L  #integer
+is.integer(a) # TRUE
+
+name<-"John"
+greeting<-"Hello,R!"
+
+flag1<-TRUE
+flag2<-FALSE
+
+z<-2+3i
+
+r<-charToRaw("R programming")
+
+class(X) #returns the class
+typeof(x) #Internal storage type
+
+"num_vec[2](returns the second element)"
+
+"num_vec* 2(multiplies each element by 2)"
+
+mat<-matrix(1:9,nrow=3,ncol=3)
+
+mat[2,3]#(row2,column3)
+
+"mat*2(multiplies all element by 2)"
